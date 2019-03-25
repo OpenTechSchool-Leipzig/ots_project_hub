1afc7f15ea8369ec6b33b0b107124669e27e4e52
Dan McAtee <info@danmcatee.com>
2019-03-25 22:30:37 +0100

    Add Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
 import React, { Fragment } from 'react';
 import styled from '@emotion/styled';
+import { Router, Link, RouteComponentProps } from '@reach/router';
 
 import 'nes.css/css/nes.css';
 
 import Header from './components/Header';
 import ProjectGrid from './components/ProjectGrid';
+import About from './components/About';
 
 const App = () => (
   <>
     <Header />
-    <ProjectGrid />
+    <Router>
+      <ProjectGrid path="/" />
+      <About path="about" />
+    </Router>
   </>
 );
 
